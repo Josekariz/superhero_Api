234c0f1e163da750510173cb21b19c532ea74c4d
Josekariz <joseph.macharia@student.moringaschool.com>
2022-12-02 10:12:48 +0300

    edit routes to power update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :hero_powers, only: :update 
-  resources :powers, only:[:index, :show, :create]
+  resources :powers, only:[:index, :show, :update]
   resources :heroes,  only:[:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
