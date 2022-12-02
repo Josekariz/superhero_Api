6a9b9f5aaa6e691dbf2b7eb170fd8026e354a9a1
Josekariz <joseph.macharia@student.moringaschool.com>
2022-12-02 09:44:47 +0300

    creatte heroe not found rescue

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,13 +1,17 @@
 class HeroesController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   def index
-   render json: Heroe.all, status: :ok
+   render json: Heroe.all,  include: ['']
   end
 
   def show
-    render json: find_hero, status: :ok
+    render json: find_hero
   end
   private 
   def find_hero
     Heroe.find(params[:id])
+  end
+  def render_not_found
+    render json: {error: "Hero not found"}, status: :not_found
   end
 end
