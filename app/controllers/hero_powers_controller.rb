f979e11f94e81057b28517c05185586010ac376d
Josekariz <joseph.macharia@student.moringaschool.com>
2022-12-02 11:27:20 +0300

    add heroe_power controller with craete heropower

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,16 @@
 class HeroPowersController < ApplicationController
+
+def create
+
+hero_powers= HeroPower.create!(hero_powers_params)
+render json:hero_powers, status: :created
+
+
+rescue ActiveRecord::RecordInvalid => e
+  render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+end
+private 
+def hero_powers_params
+  params.permit(:strength,:power_id,:heroe_id)
+end
 end
