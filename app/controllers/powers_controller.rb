358f3c7dc19163d5facdd634ff74d97884b8cadc
Josekariz <joseph.macharia@student.moringaschool.com>
2022-12-02 10:13:24 +0300

    create powers methods index, update and show

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,30 @@
 class PowersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  def index
+    render json: Power.all
+  end
+
+  def show
+    render json: find_power
+  end
+
+  def update 
+    power=find_power
+    power.update!(power_params)
+    render json: power
+  end
+
+  private 
+  def find_power
+    Power.find(params[:id])
+  end
+  def render_not_found
+    render json: {error: "Power not found"}, status: :not_found
+  end
+
+
+  def power_params
+    params.permit(:description)
+  end
 end
